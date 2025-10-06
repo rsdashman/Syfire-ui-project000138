b7a758066b0d37c74972421342e09323f2844df2
Idk345123324213 <efsadafsewdaasd@tutamail.com>
2025-10-06 15:57:11 -0300

    Create Th4msUniversal.lua

---
+++ Th4msUniversal.lua
@@ -0,0 +1,35 @@
+local WindUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Footagesus/WindUI/main/dist/main.lua"))()
+
+local Window = WindUI:CreateWindow({
+    Title = "loc:WINDUI_EXAMPLE",
+    Icon = "geist:window",
+    Author = "loc:WELCOME",
+    Folder = "WindUI_Example",
+    Size = UDim2.fromOffset(580, 490),
+    Theme = "Dark",
+    
+    HidePanelBackground = false,
+    NewElements = false,
+    -- Background = WindUI:Gradient({
+    --     ["0"] = { Color = Color3.fromHex("#0f0c29"), Transparency = 1 },
+    --     ["100"] = { Color = Color3.fromHex("#302b63"), Transparency = 0.9 },
+    -- }, {
+    --     Rotation = 45,
+    -- }),
+    --Background = "video:https://cdn.discordapp.com/attachments/1337368451865645096/1402703845657673878/VID_20250616_180732_158.webm?ex=68958a01&is=68943881&hm=164c5b04d1076308b38055075f7eb0653c1d73bec9bcee08e918a31321fe3058&",
+    User = {
+        Enabled = true,
+        Anonymous = true,
+        Callback = function()
+            WindUI:Notify({
+                Title = "User Profile",
+                Content = "User profile clicked!",
+                Duration = 3
+            })
+        end
+    },
+    Acrylic = false,
+    HideSearchBar = false,
+    SideBarWidth = 200,
+
+})
